251c442df312c010aaaf7cf1c3d2103cb5187c4b
Domenico D'Angelo <domenicodangelo@macbook-pro-di-domenico.local>
2018-03-11 21:57:41 +0100

    Initial Commit

---
+++ myEat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myEat
+//
+//  Created by Domenico D'Angelo on 11/03/18.
+//  Copyright © 2018 Domenico D'Angelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
