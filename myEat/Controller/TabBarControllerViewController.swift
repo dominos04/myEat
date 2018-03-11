427e93e10b782a0517ff260519943bb3c2c1b011
Domenico D'Angelo <domenicodangelo@mbp-di-domenico.fritz.box>
2018-03-11 22:14:44 +0100

    Manage the storyboard

---
+++ myEat/Controller/TabBarControllerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarControllerViewController.swift
+//  myEat
+//
+//  Created by Domenico D'Angelo on 11/03/18.
+//  Copyright © 2018 Domenico D'Angelo. All rights reserved.
+//
+
+import UIKit
+
+class TabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
